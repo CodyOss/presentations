db1c56ac50af937d60d08f3e90836eefeec043f4
closs <closs@spscommerce.com>
2017-05-04 14:14:25 -0500

    Make short versions shorter!

---
+++ ready-set-go/code/methods-interfaces.go
@@ -0,0 +1,32 @@
+package main
+
+import "fmt"
+
+// START OMIT
+type Pet interface { // HL
+	Name() string        // HL
+	FavoriteToy() string // HL
+}
+
+type Dog struct { // Implicitly a Pet because it fulfills the interface
+	name        string
+	favoriteToy string
+}
+
+func (d Dog) Name() string { // Value receiver
+	return d.name
+}
+func (d *Dog) FavoriteToy() string { //Pointer receiver
+	return d.favoriteToy
+}
+
+func PetAnimal(p Pet) { // HL
+	fmt.Printf("You petted %s!\n", p.Name())
+}
+
+func main() {
+	d := Dog{"Meeko", "🍖"}
+	PetAnimal(d)
+}
+
+// END OMIT
